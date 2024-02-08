900a64ce0d9c797ee1afa75682db26d509b6c255
Alday <thomasconrod@gmail.com>
2024-02-08 23:22:22 +0000

    clipboard todo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.clipboard = "unnamedplus"
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+
+--TODO => enable copy and pasting to system clipboard with a command, so you can still use the vim registry workflow.
